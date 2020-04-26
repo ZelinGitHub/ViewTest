277955a939f4a075dc5fddc0d8b14eff93e7f295
ZelinGithub <1628747456@qq.com>
2020-04-26 18:42:21 +0800

    Service

---
+++ app/src/main/java/com/fuck/viewtest/Bean/Dog.java
@@ -0,0 +1,23 @@
+package com.fuck.viewtest.Bean;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Dog implements Parcelable {
+    private String name;
+    public static final Creator<Dog> CREATOR = new DogCreator();
+
+    Dog(Parcel in) {
+        name = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+    }
+}
---
+++ app/src/main/java/com/fuck/viewtest/Bean/Wolf.java
@@ -0,0 +1,7 @@
+package com.fuck.viewtest.Bean;
+
+import java.io.Serializable;
+
+public class Wolf implements Serializable {
+    private static final long serialVersionUID=519067123721295773L;
+}
