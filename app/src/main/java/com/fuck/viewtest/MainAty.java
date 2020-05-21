b6117b569afcf4ecd574564df119057b669c8dd4
ZelinGithub <1628747456@qq.com>
2020-05-21 22:59:20 +0800

    X

--- app/src/main/java/com/fuck/viewtest/MainAty.java
+++ app/src/main/java/com/fuck/viewtest/MainAty.java
@@ -1,11 +1,11 @@
 package com.fuck.viewtest;
 
 import android.content.Intent;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.fuck.viewtest.anim.AnimAty;
 import com.fuck.viewtest.broadcast.BroadcastAty;
@@ -14,6 +14,8 @@ import com.fuck.viewtest.serv.client.ServAty;
 import com.fuck.viewtest.socket.client.SocketAty;
 import com.fuck.viewtest.vp.VpAty;
 import com.fuck.viewtest.win.WinAty;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 //ABC
 public class MainAty extends AppCompatActivity implements View.OnClickListener {
@@ -54,6 +56,8 @@ public class MainAty extends AppCompatActivity implements View.OnClickListener {
         btn_socket.setOnClickListener(this);
         btn_cusv.setOnClickListener(this);
 
+        ShapeDrawable shapeDrawable;
+        LayerDrawable layerDrawable;
     }
 
     @Override
