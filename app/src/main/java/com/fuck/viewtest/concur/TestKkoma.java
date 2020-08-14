139ad5d229da8c7ba624e5bb5c03fbc4302f45fc
ZelinGitHub <2414217563@qq.com>
2020-08-14 19:06:55 +0800

    dsfasdfsa

--- app/src/main/java/com/fuck/viewtest/concur/TestKkoma.java
+++ app/src/main/java/com/fuck/viewtest/concur/TestKkoma.java
@@ -5,5 +5,10 @@ import android.os.MessageQueue;
 public class TestKkoma {
     public static void test(){
         MessageQueue messageQueue;
+
+        MyAsyncTask asyncTask=new MyAsyncTask();
+        asyncTask.execute("Faker");
+        asyncTask.cancel(true);
     }
+
 }
