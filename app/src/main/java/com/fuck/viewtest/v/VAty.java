be4ff5e5e2b84d603251083b26462374b40a59d3
ZelinGitHub <2414217563@qq.com>
2020-12-21 19:07:32 +0800

    serial

--- app/src/main/java/com/fuck/viewtest/v/VAty.java
+++ app/src/main/java/com/fuck/viewtest/v/VAty.java
@@ -36,28 +36,28 @@ public class VAty extends AppCompatActivity {
     }
 
     private void moveView() {
-        System.out.println("View原始坐标："+v.getLeft()+" "+v.getTop()+" "+v.getRight()+" "+v.getBottom());
-        System.out.println("View当前坐标："+v.getX()+" "+v.getY());
+        System.out.println("View原始坐标：" + v.getLeft() + " " + v.getTop() + " " + v.getRight() + " " + v.getBottom());
+        System.out.println("View当前坐标：" + v.getX() + " " + v.getY());
         v.setX(500);
         v.setY(500);
-        System.out.println("View原始坐标："+v.getLeft()+" "+v.getTop()+" "+v.getRight()+" "+v.getBottom());
-        System.out.println("View当前坐标："+v.getX()+" "+v.getY());
+        System.out.println("View原始坐标：" + v.getLeft() + " " + v.getTop() + " " + v.getRight() + " " + v.getBottom());
+        System.out.println("View当前坐标：" + v.getX() + " " + v.getY());
         v.setTranslationX(200);
         v.setTranslationY(200);
-        System.out.println("View原始坐标："+v.getLeft()+" "+v.getTop());
-        System.out.println("View当前坐标："+v.getX()+" "+v.getY());
+        System.out.println("View原始坐标：" + v.getLeft() + " " + v.getTop());
+        System.out.println("View当前坐标：" + v.getX() + " " + v.getY());
         v.setTranslationX(200);
         v.setTranslationY(1200);
-        System.out.println("View原始坐标："+v.getLeft()+" "+v.getTop());
-        System.out.println("View当前坐标："+v.getX()+" "+v.getY());
+        System.out.println("View原始坐标：" + v.getLeft() + " " + v.getTop());
+        System.out.println("View当前坐标：" + v.getX() + " " + v.getY());
 
         v.offsetLeftAndRight(300);
         v.offsetTopAndBottom(300);
-        System.out.println("View原始坐标："+v.getLeft()+" "+v.getTop());
-        System.out.println("View当前坐标："+v.getX()+" "+v.getY());
+        System.out.println("View原始坐标：" + v.getLeft() + " " + v.getTop());
+        System.out.println("View当前坐标：" + v.getX() + " " + v.getY());
 
-        int width = ViewGroup.LayoutParams.WRAP_CONTENT;
-        int height=ViewGroup.LayoutParams.MATCH_PARENT;
-        ViewGroup.LayoutParams layoutParams=new ViewGroup.LayoutParams(width,height);
     }
+
+
+
 }
