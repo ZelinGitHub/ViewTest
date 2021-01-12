e58a79b035b95d0e4ea782f45ae4746791ed1e24
ZelinGitHub <2414217563@qq.com>
2021-01-12 16:59:51 +0800

    parcel

--- app/src/main/java/com/fuck/viewtest/seria/Dog.java
+++ app/src/main/java/com/fuck/viewtest/seria/Dog.java
@@ -3,12 +3,20 @@ package com.fuck.viewtest.seria;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Dog implements Parcelable {
-    private String name;
+    public int age;
+    public String name;
     public static final Creator<Dog> CREATOR = new DogCreator();
+
+    Dog() {
+
+    }
 
     Dog(Parcel in) {
         name = in.readString();
+        age = in.readInt();
     }
 
     @Override
@@ -19,5 +27,16 @@ public class Dog implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
+        dest.writeInt(age);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", hashCode='" + hashCode() + '\'' +
+                '}';
     }
 }
--- app/src/main/java/com/fuck/viewtest/seria/SerialAty.java
+++ app/src/main/java/com/fuck/viewtest/seria/SerialAty.java
@@ -1,6 +1,7 @@
 package com.fuck.viewtest.seria;
 
 import android.os.Bundle;
+import android.os.Parcel;
 import android.view.View;
 import android.widget.Button;
 
@@ -8,15 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.fuck.viewtest.R;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 public class SerialAty extends AppCompatActivity implements View.OnClickListener {
-    private Button btn_serial;
-    private Button btn_anti_serial;
+    private Button btn_exe_serial;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +23,39 @@ public class SerialAty extends AppCompatActivity implements View.OnClickListener
 
 
     private void initViews() {
-        btn_serial = findViewById(R.id.btn_serial);
-        btn_anti_serial = findViewById(R.id.btn_anti_serial);
+        btn_exe_serial = findViewById(R.id.btn_exe_serial);
     }
 
     private void initUI() {
-        btn_serial.setOnClickListener(this);
-        btn_anti_serial.setOnClickListener(this);
+        btn_exe_serial.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_serial: {
-                break;
-            }
-            case R.id.btn_anti_serial: {
+            case R.id.btn_exe_serial: {
+                serial();
                 break;
             }
         }
+    }
+
+    private void serial() {
+        //创建一个对象
+        Dog dog = new Dog();
+        dog.age = 6;
+        dog.name = "Spike";
+        System.out.println("序列化的对象 " + dog.toString());
+        //获得一个Parcel
+        Parcel parcel = Parcel.obtain();
+        //写出对象到Parcel
+        dog.writeToParcel(parcel, 0);
+        //从Parcel创建对象
+        Dog dog_ = Dog.CREATOR.createFromParcel(parcel);
+        System.out.println("反序列化的对象 " + dog_.toString());
+        //回收Parcel
+        parcel.recycle();
     }
 
 
