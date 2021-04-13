dfa1942cc2c49cde2dfccd61ecfd77ef4576628d
ZelinGitHub <2414217563@qq.com>
2021-04-13 19:01:52 +0800

    OkHttp

--- app/src/main/java/com/fuck/viewtest/menu/net/okhttp/x509trustmgr/SelfX509TrustMgr.java
+++ app/src/main/java/com/fuck/viewtest/menu/net/okhttp/x509trustmgr/SelfX509TrustMgr.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.util.Arrays;
 import java.util.Collection;
@@ -19,53 +23,57 @@ public class SelfX509TrustMgr {
     //获取信任自签名证书的X509TrustManager
     private X509TrustManager trustManagerForCertificates(InputStream in)
             throws GeneralSecurityException {
-        //得到证书工厂
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-        //使用证书工厂，得到自签名证书集合
-        Collection<? extends Certificate> certificates=certificateFactory.generateCertificates(in);
-
-        if (certificates.isEmpty()) {
+        //从输入流得到证书
+        Collection<? extends Certificate> certificates = getCertificates(in);
+        if (!certificates.isEmpty()) {
+            //密钥库的密码
+            char[] password = "123456".toCharArray();
+            //得到密钥库
+            KeyStore keyStore = getEmptyKeyStore(password);
+            //导入证书到密钥库
+            importCertificatesToKeyStore(certificates, keyStore);
+            //初始化KeyManagerFactory
+            initKeyManagerFactory(password, keyStore);
+            //初始化TrustManagerFactory
+            TrustManagerFactory trustManagerFactory = initTrustManagerFactory(keyStore);
+            //得到TrustManager数组
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            if(trustManagers.length==1){
+                //得到TrustManager
+                TrustManager trustManager = trustManagers[0];
+                if(trustManager instanceof X509TrustManager){
+                    //得到TrustManager
+                    return (X509TrustManager) trustManager;
+                }else {
+                    throw new IllegalStateException("Unexpected default trust managers:"
+                            + Arrays.toString(trustManagers));
+                }
+            }else {
+                throw new IllegalStateException("Unexpected default trust managers:"
+                        + Arrays.toString(trustManagers));
+            }
+        } else {
             throw new IllegalArgumentException("expected non-empty set of trusted certificates");
         }
-
-        //密钥库的密码
-        char[] password = "password".toCharArray();
-        //得到密钥库
-        KeyStore keyStore = getEmptyKeyStore(password);
-        int index = 0;
-        //遍历证书数组
-        for (Certificate certificate : certificates) {
-            //使用索引值作为证书的别名
-            String certificateAlias = Integer.toString(index++);
-            //把证书导入密钥库中
-            keyStore.setCertificateEntry(certificateAlias, certificate);
-        }
-
-        //得到密钥管理器工厂
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
-                KeyManagerFactory.getDefaultAlgorithm());
-        //初始化密钥管理器工厂
-        //参数1传入密钥库
-        //参数2传入密钥库的密码
-        keyManagerFactory.init(keyStore, password);
-
-
-        //得到TrustManagerFactory实例
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-                TrustManagerFactory.getDefaultAlgorithm());
-        //初始化TrustManagerFactory，参数传入密钥库
-        trustManagerFactory.init(keyStore);
-        //得到TrustManager数组
-        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-
-        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-            throw new IllegalStateException("Unexpected default trust managers:"
-                    + Arrays.toString(trustManagers));
-        }
-        return (X509TrustManager) trustManagers[0];
     }
 
 
+
+
+    /*
+    得到证书文件对应的Certificate实例集合
+     */
+    private Collection<? extends Certificate> getCertificates(InputStream in) throws CertificateException {
+        //得到证书工厂
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        //使用证书工厂，得到证书集合
+        return certificateFactory.generateCertificates(in);
+    }
+
+
+    /*
+    得到空的密钥库
+     */
     private KeyStore getEmptyKeyStore(char[] password) throws GeneralSecurityException {
         try {
             //得到KeyStore实例
@@ -79,4 +87,44 @@ public class SelfX509TrustMgr {
             throw new AssertionError(e);
         }
     }
+
+    /*
+    导入证书到密钥库
+     */
+    private void importCertificatesToKeyStore(Collection<? extends Certificate> pCertificates, KeyStore pKeyStore) throws KeyStoreException {
+        int index = 0;
+        //遍历证书数组
+        for (Certificate certificate : pCertificates) {
+            //使用索引值作为证书的别名
+            String certificateAlias = Integer.toString(index++);
+            //把证书导入密钥库中
+            pKeyStore.setCertificateEntry(certificateAlias, certificate);
+        }
+    }
+
+    /*
+    初始化KeyManagerFactory
+     */
+    private void initKeyManagerFactory(char[] pPassword, KeyStore pKeyStore) throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+        //得到密钥管理器工厂
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
+                KeyManagerFactory.getDefaultAlgorithm());
+        //初始化密钥管理器工厂
+        //参数1传入密钥库
+        //参数2传入密钥库的密码
+        keyManagerFactory.init(pKeyStore, pPassword);
+    }
+
+    /*
+    初始化TrustManagerFactory
+     */
+    private TrustManagerFactory initTrustManagerFactory(KeyStore pKeyStore) throws NoSuchAlgorithmException, KeyStoreException {
+        //得到TrustManagerFactory实例
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                TrustManagerFactory.getDefaultAlgorithm());
+        //初始化TrustManagerFactory，参数传入密钥库
+        trustManagerFactory.init(pKeyStore);
+        return trustManagerFactory;
+    }
+
 }
---
+++ app/src/main/java/com/fuck/viewtest/menu/net/retrofit/RequestAPI.java
@@ -0,0 +1,37 @@
+package com.fuck.viewtest.menu.net.retrofit;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+
+public interface RequestAPI {
+    @GET("www.baidu.com")
+    Call<ResponseBody> getData(@Query("id") long id, @Query("name") String name);
+
+
+    @FormUrlEncoded
+    @POST("www.qq.com")
+    Call<ResponseBody> die(
+            @Field("id") long id
+            , @Field("name") String name
+            , @Field("sex") String sex
+    );
+
+
+    @Multipart
+    @POST("www.fuck.com")
+    Call<ResponseBody> fuck(
+            @Part("girl") RequestBody girl
+            , @Part MultipartBody.Part part
+    );
+
+
+}
