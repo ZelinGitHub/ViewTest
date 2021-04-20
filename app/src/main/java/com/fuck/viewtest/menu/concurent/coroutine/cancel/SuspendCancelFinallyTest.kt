3fcdaaaf77558c7f58d85f9186fb6f95d990ea5d
ZelinGithub <1628747456@qq.com>
2021-04-20 22:40:43 +0800

    Kotlin

--- app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/cancel/SuspendCancelFinallyTest.kt
+++ app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/cancel/SuspendCancelFinallyTest.kt
@@ -1,9 +1,6 @@
 package com.fuck.viewtest.menu.concurent.coroutine.cancel
 
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun testDelayCancelFinally() {
     Thread {
@@ -40,6 +37,31 @@ fun testDelayCancelFinallyDelay() {
                     println("协程2 再次挂起")
                     delay(5000L)
                     println("协程2 执行结束")
+                }
+            }
+            delay(2000L) // 延迟一段时间
+            println("协程1 取消协程2")
+            job.cancelAndJoin() // 取消协程2，等待协程2执行结束
+            println("协程1 执行结束")
+        }
+    }.start()
+}
+fun testDelayCancelFinallyDelayWithContext() {
+    Thread {
+        runBlocking {
+            println("协程1 开始执行")
+            val job = this.launch {
+                println("协程2 开始执行")
+                try{
+                    println("协程2 挂起")
+                    delay(5000L)
+                }finally {
+                    println("协程2 进入finally")
+                    withContext(NonCancellable){
+                        println("协程2 再次挂起")
+                        delay(5000L)
+                        println("协程2 执行结束")
+                    }
                 }
             }
             delay(2000L) // 延迟一段时间
