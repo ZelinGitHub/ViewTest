2b2c48082e3abdccdd0946a4f4ab069a954e84b1
ZelinGitHub <2414217563@qq.com>
2021-04-21 18:36:16 +0800

    Coroutine

--- app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/cancel/SuspendCancelFinallyTest.kt
+++ app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/cancel/SuspendCancelFinallyTest.kt
@@ -2,17 +2,28 @@ package com.fuck.viewtest.menu.concurent.coroutine.cancel
 
 import kotlinx.coroutines.*
 
-fun testDelayCancelFinally() {
+
+fun testDelayCancelFinallyDelayWithContext() {
     Thread {
         runBlocking {
             println("协程1 开始执行")
             val job = this.launch {
                 println("协程2 开始执行")
-                try{
+                try {
                     println("协程2 挂起")
                     delay(5000L)
-                }finally {
+                } finally {
+                    println("协程2 进入finally")
+                    println("协程2 活跃状态 ${this.isActive}")
+                    withContext(NonCancellable) {
+                        println("withContext协程 开始执行")
+                        println("withContext协程 活跃状态 ${this.isActive}")
+                        println("withContext协程 再次挂起")
+                        delay(5000L)
+                        println("withContext协程 执行结束")
+                    }
                     println("协程2 执行结束")
+
                 }
             }
             delay(2000L) // 延迟一段时间
@@ -23,51 +34,3 @@ fun testDelayCancelFinally() {
     }.start()
 }
 
-fun testDelayCancelFinallyDelay() {
-    Thread {
-        runBlocking {
-            println("协程1 开始执行")
-            val job = this.launch {
-                println("协程2 开始执行")
-                try{
-                    println("协程2 挂起")
-                    delay(5000L)
-                }finally {
-                    println("协程2 进入finally")
-                    println("协程2 再次挂起")
-                    delay(5000L)
-                    println("协程2 执行结束")
-                }
-            }
-            delay(2000L) // 延迟一段时间
-            println("协程1 取消协程2")
-            job.cancelAndJoin() // 取消协程2，等待协程2执行结束
-            println("协程1 执行结束")
-        }
-    }.start()
-}
-fun testDelayCancelFinallyDelayWithContext() {
-    Thread {
-        runBlocking {
-            println("协程1 开始执行")
-            val job = this.launch {
-                println("协程2 开始执行")
-                try{
-                    println("协程2 挂起")
-                    delay(5000L)
-                }finally {
-                    println("协程2 进入finally")
-                    withContext(NonCancellable){
-                        println("协程2 再次挂起")
-                        delay(5000L)
-                        println("协程2 执行结束")
-                    }
-                }
-            }
-            delay(2000L) // 延迟一段时间
-            println("协程1 取消协程2")
-            job.cancelAndJoin() // 取消协程2，等待协程2执行结束
-            println("协程1 执行结束")
-        }
-    }.start()
-}
