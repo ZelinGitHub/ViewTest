046694ef99fa40940e00988aa82a41eb43fb5ce3
ZelinGitHub <2414217563@qq.com>
2021-04-23 18:57:37 +0800

    Kotlin

--- app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/CoroutineTest.kt
+++ app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/CoroutineTest.kt
@@ -1,12 +1,10 @@
 package com.fuck.viewtest.menu.concurent.coroutine
 
 import com.fuck.viewtest.menu.concurent.coroutine.cancel.*
-import com.fuck.viewtest.menu.concurent.coroutine.coroutine.normalcoroutine.startCoroutineInBlockingCoroutineOnGlobalScope
-import com.fuck.viewtest.menu.concurent.coroutine.coroutine.normalcoroutine.startCoroutineInBlockingCoroutineOnNewScope
-import com.fuck.viewtest.menu.concurent.coroutine.coroutine.normalcoroutine.startCoroutineInBlockingCoroutineOnParamScope
-import com.fuck.viewtest.menu.concurent.coroutine.coroutine.normalcoroutine.testCoroutineWeigh
+import com.fuck.viewtest.menu.concurent.coroutine.cancel.testDelayCancelFinallyDelayWithContext
+import com.fuck.viewtest.menu.concurent.coroutine.coroutine.normalcoroutine.*
 
 
 fun testCoroutine(){
-    testDelayCancelFinallyDelayWithContext()
+    startDeferredCoroutineInThreadOnGlobalScope()
 }
--- app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/coroutine/normalcoroutine/StartCoroutineInThread.kt
+++ app/src/main/java/com/fuck/viewtest/menu/concurent/coroutine/coroutine/normalcoroutine/StartCoroutineInThread.kt
@@ -23,7 +23,7 @@ fun startCoroutineInThreadOnGlobalScope() {
 }
 
 //协程先执行结束，线程后执行结束
-fun startCoroutineInThread2OnGlobalScope() {
+fun startCoroutineInThreadOnGlobalScope2() {
     Thread {
         println("线程开始执行")
         //启动一个协程
@@ -36,6 +36,26 @@ fun startCoroutineInThread2OnGlobalScope() {
         Thread.sleep(3000)
         // 协程在等待时，线程还在执行
         println("线程执行结束")
+    }.start()
+}
+
+//
+fun startDeferredCoroutineInThreadOnGlobalScope() {
+    Thread {
+        println("线程 开始执行")
+        runBlocking {
+            println("阻塞块线程 开始执行")
+            val deferred: Deferred<String> = GlobalScope.async {
+                println("异步协程 开始执行")
+                delay(3000L)
+                println("异步协程 执行结束")
+                "Death"
+            }
+            val result = deferred.await()
+            println("阻塞块协程 异步协程的执行结果是 $result")
+            println("阻塞块协程 执行结束")
+        }
+        println("线程 执行结束")
     }.start()
 }
 
